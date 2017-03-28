3b36a83ace1122a558cc839c9febe320c607e174
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2017-03-28 14:15:38 -0400

    Lab6 code nearly done; just need to prove accuracy for part 1

--- L6/tise_fd.m
+++ L6/tise_fd.m
@@ -2,25 +2,26 @@ function [eigE, eigfuncs, x] = tise_fd(V, x0, xend, N, Ne, numret)
 %finite differences for time-independent schroedinger equation
 % assuming psi(x0) = 0 = psi(xend)
 m2_hsqr = 2 / 0.076199682;  % eV^-1 nm^-2
+
 % linspace is the most reliable way to make N evenly spaced points.
 % Jacob's code in C16_3 would be wrong for non-integer x0 and xend
 x = linspace(x0, xend, N+2);
 x = x(2:end-1);
 h = x(2) - x(1);
 
-D = 2/h^2 * eye(N) + m2_hsqr * diag(V(x));  % diagonal
-D = D - 1/h^2 * diag(ones(N-1,1),1);  % upper
-D = D - 1/h^2 * diag(ones(N-1,1),-1);  % lower
-D = sparse(D);
+% I'll generate the sparse matrix directly for efficiency
+offdiag = -1/h^2 * ones(1,N-1);
+diags = [offdiag,0; 2/h^2 + m2_hsqr*V(x); 0,offdiag].';
+D = spdiags(diags, [-1,0,1], N, N);
 
 [eigfuncs, e] = eigs(D, Ne, 'sa');
 eigE = diag(e);
-eigE = eigE(end-numret+1) ./ m2_hsqr;
+eigE = eigE(end-numret+1:end) ./ m2_hsqr;
 eigfuncs = eigfuncs(:,end-numret+1:end).';
-for i = [1:numret]
+for i = [1:numret]  % normalize eigenfunctions
     C = discrete_trap(eigfuncs(i,:).^2, x)^-0.5;
-    if (eigfuncs(i,floor(0.5*N)) < 0)
-        C = -C;
+    if (eigfuncs(i,floor(0.5*N)-1) < 0)
+        C = -C;  % choose wavefunction positive left of centre
     end
     eigfuncs(i,:) = C * eigfuncs(i,:);
 end
